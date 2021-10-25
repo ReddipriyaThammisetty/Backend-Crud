6d7d3dfb1834d832e2286044b5f9b7393762ad06
Reddi priya Thammisetty <92013566+ReddipriyaThammisetty@users.noreply.github.com>
2021-10-25 18:44:35 +0530

    Add files via upload

---
+++ StudentRegistrationform/src/main/java/com/example/StudentRegistrationform/StudentRegistrationformApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentRegistrationform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistrationformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistrationformApplication.class, args);
+	}
+
+}
---
+++ StudentRegistrationform/src/test/java/com/example/StudentRegistrationform/StudentRegistrationformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentRegistrationform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRegistrationformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
